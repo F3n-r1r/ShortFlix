38e36af41f22a2d35c7d431f191289852f5fdcd7
Benjamin Lund <lund.b92@gmail.com>
2019-11-09 13:33:04 +0100

    Client - Added fonts and created method to automatically set authorization header and use axios on http requests

--- client/src/main.js
+++ client/src/main.js
@@ -2,8 +2,18 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import axios from 'axios';
 
 Vue.config.productionTip = false
+
+// Automatically use Axios in component when we do this.$http
+Vue.prototype.$http = axios;
+// Get token from localstorage
+const token = localStorage.getItem('token');
+// Set Authorization header to token, so its automatically send with every request
+if(token) {
+  Vue.prototype.$http.defaults.headers.common['Authorization'] = token;
+}
 
 new Vue({
   router,
