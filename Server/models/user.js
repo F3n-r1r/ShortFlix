464f478a47408b00474401bc209d4b1597cd6cb9
Benjamin Lund <lund.b92@gmail.com>
2019-11-09 10:42:15 +0100

    Server - Added user model & pre save password encryption

---
+++ Server/models/user.js
@@ -0,0 +1,93 @@
+/*--------------------------------------------------------------------------------------------------*\
+    INDEX:
+        1. - FILE NOTES
+        2. - REQUIRE DEPENDENCIES
+        3. - USER SCHEMA
+        4. - PRE SAVE FUNCTION
+        5. - MODULE EXPORTS
+\*--------------------------------------------------------------------------------------------------*/
+
+
+/*-------------------------------------------------*\
+    1. - FILE NOTES
+\*-------------------------------------------------*/
+
+
+
+/*-------------------------------------------------*\
+    2. - REQUIRE DEPENDENCIES
+\*-------------------------------------------------*/
+const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
+const { saltrounds } = require('../config/envConfig');
+const { validateEmail } = require('../utils/validate');
+
+
+/*-------------------------------------------------*\
+    3. - USER SCHEMA
+\*-------------------------------------------------*/
+const userSchema = new mongoose.Schema({
+    email: {
+        type: String,
+        required: true,
+        unique: true,
+        validate: [
+            validateEmail
+        ],
+        trim: true,
+        lowercase: true
+    },
+    password: {
+        type: String,
+        required: true,
+        trim: true,
+        minLength: 8
+    },
+    firstname: {
+        type: String,
+        required: true,
+        trim: true,
+        lowercase: true
+    },
+    lastname: {
+        type: String,
+        required: true,
+        trim: true,
+        lowercase: true
+    },
+    role: {
+        type: String,
+        enum: [
+            'User',
+            'Admin'
+        ],
+        default: 'User'
+    }
+})
+
+
+/*-------------------------------------------------*\
+    4. - PRE SAVE FUNCTION
+\*-------------------------------------------------*/
+userSchema.pre('save', function(next) {
+    let user = this;
+    if(!user.isModified('password')) {
+        return next();
+    } else {
+        let salt = bcrypt.genSaltSync(parseInt(saltrounds));
+        bcrypt.hash(user.password, salt, (err, hash) => {
+            if(err) {
+                return next(err);
+            } else {
+                user.password = hash;
+                next();
+            }
+        })
+    }
+})
+
+
+/*-------------------------------------------------*\
+    5. - MODULE EXPORTS
+\*-------------------------------------------------*/
+module.exports = mongoose.model('User', userSchema);
