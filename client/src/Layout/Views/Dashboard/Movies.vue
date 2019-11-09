75acedcee6d4b8decec6c60bd305fe6df2fb1627
Benjamin Lund <lund.b92@gmail.com>
2019-11-09 14:34:22 +0100

    Client - Added initial views, components, layouts & new folder structure

---
+++ client/src/Layout/Views/Dashboard/Movies.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="view movies-view">
+    <p>movies</p>
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'Movies',
+  components: {
+
+  }
+}
+</script>
+
+
+
+<style lang="scss">
+
+
+</style>
---
+++ client/src/Layout/Views/Dashboard/Profile.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="view profile-view">
+    <p>Profile</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Profile',
+  components: {
+
+  }
+}
+</script>
+
+
+<style lang="scss">
+
+</style>
