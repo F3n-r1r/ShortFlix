2e6f38f5372c5335dcae72df043689b46f219fb2
Benjamin Lund <lund.b92@gmail.com>
2019-11-09 11:31:01 +0100

    Server - Added route to handle user login

---
+++ Server/routes/user/_login.js
@@ -0,0 +1,57 @@
+/*--------------------------------------------------------------------------------------------------*\
+    INDEX:
+        1. - FILE NOTES
+        2. - REQUIRE DEPENDENCIES
+        3. - REGISTER USER ROUTE FUNCTION
+        4. - MODULE EXPORTS
+\*--------------------------------------------------------------------------------------------------*/
+
+
+/*-------------------------------------------------*\
+    1. - FILE NOTES
+\*-------------------------------------------------*/
+
+
+/*-------------------------------------------------*\
+    2. - REQUIRE DEPENDENCIES
+\*-------------------------------------------------*/
+const express = require('express');
+const router = express.Router();
+const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken')
+const user = require('../../models/user');
+const { jwtkey } = require('../../config/envConfig');
+
+
+/*-------------------------------------------------*\
+    3. - LOGIN USER ROUTE FUNCTION
+\*-------------------------------------------------*/
+router.post('/', function(req, res) {
+    user.findOne({
+        email: req.body.email
+    }).then((user) => {
+        if(bcrypt.compareSync(req.body.password, user.password)) {
+            const token = jwt.sign({id: user._id}, jwtkey, { expiresIn: '24h' });
+            res.json({
+                message: 'success',
+                token: token,
+                user: user
+            })
+        } else {
+            res.json({
+                message: 'error_password'
+            })
+        }
+    }).catch(err => {
+        res.json({
+            message: 'error_email',
+            error: err
+        })
+    });
+});
+
+
+/*-------------------------------------------------*\
+    4. - MODULE EXPORTS
+\*-------------------------------------------------*/
+module.exports = router;
