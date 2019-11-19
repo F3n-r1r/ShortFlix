7d4435c7dc9a439ecb68332b1f34f7a7aa01efce
Benjamin Lund <lund.b92@gmail.com>
2019-11-19 14:02:55 +0100

    Client - Added functionality in Network view to see personal network and pending requests

--- client/src/Layout/Views/Dashboard/Network.vue
+++ client/src/Layout/Views/Dashboard/Network.vue
@@ -1,11 +1,22 @@
 <template>
   <div class="view network-view">
-    <p>network panel</p>
 
+
+
+    <p>PENDING REQUESTS</p>
     <ul>
-        <li v-for="(user, index) in users" :key="index">
+        <li v-for="(user, index) in pendingRequests" :key="index">
             {{ index }} - {{ user.firstname }} - {{ user.role }}
-            <button @click="networkRequest(user._id)">Approve</button>
+            <button @click="acceptRequest(user._id)">Accept Request</button>
+        </li>
+    </ul>
+
+
+    <br><br>
+    <p>MY NETWORK</p>
+    <ul>
+        <li v-for="(user, index) in network" :key="index">
+            {{ index }} - {{ user.firstname }} - {{ user.role }}
         </li>
     </ul>
 
@@ -23,19 +34,20 @@ export default {
     },
     data() {
         return {
-            users: {}
+            network: [],
+            pendingRequests: []
         }
     },
     methods: {
-        networkRequest: function(userId) {
+        acceptRequest: function(userId) {
             return new Promise((resolve, reject) => {
                 let data = {
-                    userA: this.$store.getters.user._id,
-                    userB: userId
+                    id: userId
                 }
-                axios({method: 'POST', url: 'http://localhost:8000/api/user/network/request', data: data })
+                axios({method: 'POST', url: 'http://localhost:8000/api/user/network/accept', data: data })
                 .then(resp => {
                     console.log(resp)
+                    this.pendingUsers = this.pendingUsers.filter(x => x._id !== userId);
                     resolve(resp);
                 }).catch(err => {
                     reject(err);
@@ -45,9 +57,11 @@ export default {
     },
     created: function() {
         return new Promise((resolve, reject) => {
-            axios({method: 'GET', url: 'http://localhost:8000/api/user/all', data: this.$store.getters.user})
+            axios({method: 'GET', url: 'http://localhost:8000/api/user/network/all'})
             .then(resp => {
-                this.users = resp.data.users;
+                //console.log(resp.data)
+                this.pendingRequests = resp.data.user.network.pending;
+                this.network = resp.data.user.network.accepted;
                 resolve(resp);
             }).catch(err => {
                 reject(err);
