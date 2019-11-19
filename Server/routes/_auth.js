453daf37e201d8f57a7e64b2f53fd70ddacbfe75
Benjamin Lund <lund.b92@gmail.com>
2019-11-19 14:06:30 +0100

    Server - Moved routes for getting and approving pending users to auth instead of user

--- Server/routes/_auth.js
+++ Server/routes/_auth.js
@@ -2,8 +2,11 @@
     INDEX:
         1. - FILE NOTES
         2. - REQUIRE DEPENDENCIES
-        3. - REGISTER USER ROUTE FUNCTION
-        4. - MODULE EXPORTS
+        3. - LOGIN USER ROUTE
+        4. - REGISTER USER ROUTE
+        5. - GET PENDING USERS ROUTE
+        6. - APPROVE USER ROUTE
+        7. - MODULE EXPORTS
 \*--------------------------------------------------------------------------------------------------*/
 
 
@@ -20,6 +23,7 @@ const router = express.Router();
 const bcrypt = require('bcrypt');
 const jwt = require('jsonwebtoken')
 const user = require('../models/user');
+const network = require('../models/network');
 const { jwtkey } = require('../config/envConfig');
 
 
@@ -79,6 +83,56 @@ router.post('/register', function(req, res) {
 
 
 /*-------------------------------------------------*\
-    4. - MODULE EXPORTS
+    5. - GET PENDING USERS ROUTE
+\*-------------------------------------------------*/
+router.get('/pending', function(req, res) {
+    console.log(req.body)
+    user.find(
+        { approved: false }
+    )
+    .then((user) => {
+        res.status(200).json({
+            user
+        })
+    }).catch(err => {
+        res.json({
+            error: err
+        })
+    })
+});
+
+
+
+
+/*-------------------------------------------------*\
+    6. - APPROVE USER ROUTE
+\*-------------------------------------------------*/
+router.post('/approve', function(req, res) {
+    network.create ({})
+    .then(network => {
+        user.findOneAndUpdate(
+            { _id: req.body.id },
+            { $set: { approved: true, network: network._id } },
+            { new: true }
+        )
+        .then((user) => {
+            res.status(200).json({
+                status: 200,
+                message: 'User approved'
+            })
+        }).catch(err => {
+            res.json({
+                error: err
+            })
+        })
+    })
+
+
+});
+
+
+
+/*-------------------------------------------------*\
+    7. - MODULE EXPORTS
 \*-------------------------------------------------*/
 module.exports = router;
