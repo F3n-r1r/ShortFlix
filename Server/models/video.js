898b0bd2d64028f182eaf35328c97e5fb7e13c9e
Benjamin Lund <lund.b92@gmail.com>
2019-12-02 16:33:12 +0100

    Server - Added preliminary video model

---
+++ Server/models/video.js
@@ -0,0 +1,46 @@
+/*--------------------------------------------------------------------------------------------------*\
+    INDEX:
+        1. - FILE NOTES
+        2. - REQUIRE DEPENDENCIES
+        3. - VIDEO SCHEMA
+        4. - MODULE EXPORTS
+\*--------------------------------------------------------------------------------------------------*/
+
+
+/*-------------------------------------------------*\
+    1. - FILE NOTES
+\*-------------------------------------------------*/
+
+
+/*-------------------------------------------------*\
+    2. - REQUIRE DEPENDENCIES
+\*-------------------------------------------------*/
+const mongoose = require('mongoose');
+
+
+/*-------------------------------------------------*\
+    3. - USER SCHEMA
+\*-------------------------------------------------*/
+const videoSchema = new mongoose.Schema({
+    user: {
+        type: mongoose.Schema.Types.ObjectId, 
+        ref: 'User',
+        required: true
+    },
+    thumbnail: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    video: {
+        type: String, 
+        required: true,
+        unique: true
+    }
+})
+
+
+/*-------------------------------------------------*\
+    4. - MODULE EXPORTS
+\*-------------------------------------------------*/
+module.exports =  mongoose.model('Video', videoSchema, 'videos');
