bc877685b83d71a9e9b32e250169cd77564453fd
Benjamin Lund <lund.b92@gmail.com>
2019-12-03 12:17:48 +0100

    Client - Added functionallity for multiple file upload & further file validation & some preliminary styling

--- client/src/Layout/Components/Dashboard/Upload.vue
+++ client/src/Layout/Components/Dashboard/Upload.vue
@@ -4,36 +4,44 @@
             <!-------------------------------------------------------------------------------------->
             <!-- ??      															  -->
             <!-------------------------------------------------------------------------------------->
-     
+            <form v-if="!uploading && !uploadComplete" @submit.prevent="sendFile" class="upload__form" enctype="multipart/form-data">
+                <div class="form__field">
+                    <input class="field__input" v-model="title" type="text" placeholder="Movie title..." required>
+                </div>
+                <div class="form__field">
+                    <textarea class="field__textarea" v-model="description" name="" placeholder="Movie description..." required/>
+                </div>
+                <p class="form__info">
+                    * Upload your movie along with the related movie poster below. The poster is a requirement.
+                </p>
+                <div class="form__dropzone">
+                    <input multiple ref="files" @change="selectFile" type="file" class="dropzone__input">
+                    <p class="dropzone__cta">Drag files here</p>
+                </div>
 
-            
-                <form v-if="!uploading && !uploadComplete" @submit.prevent="sendFile" class="upload__form" enctype="multipart/form-data">
-                    <div class="form__field">
-                        <input v-model="title" type="text" placeholder="Movie title..." required>
-                    </div>
-                    <div class="form__field">
-                        <textarea v-model="description" name="" required/>
-                    </div>
-                    <div class="form__dropzone">
-                        <input ref="file" @change="selectFile" type="file" class="dropzone__input">
-                        <p v-if="!file" class="dropzone__cta">Drag files here</p>
-                        <p v-if="file">{{file.name}}</p>
-                    </div>
-                    <div v-if="message" :class="`message-${error ? 'error' : 'succes'}`">
-                        {{message}}
-                    </div>
-                    <button type="submit">Upload</button>
-                </form>
+                <div v-for="(file, index) in files" :key="index" :class="`form__files form__files--${file.invalidMessage ? 'error' : 'success'}`">
+                    <p>{{file.name}} <span v-if="file.invalidMessage">- ({{file.invalidMessage}})</span></p>
+                    <button class="files__delete" @click.prevent="files.splice(index, 1);uploadFiles.splice(index, 1)">X</button>
+                </div>
+
+                <div v-if="message && !uploadComplete">
+                    {{message}}
+                </div>
+
+
+                <button v-if="uploadRdy" class="form__submit" type="submit">Upload</button>
+            </form>
+
 
           
-                <progress v-if="uploading" class="dropzone__progress" :value="progress" max="100">
-                    {{progress}}
-                </progress>
-                
-           
-                <div v-if="uploadComplete">
-                    YOUR UPLOAD IS COMPLETE
-                </div>
+            <progress v-if="uploading" class="dropzone__progress" :value="progress" max="100">
+                {{progress}}
+            </progress>
+            
+
+            <div v-if="message && uploadComplete">
+                {{message}}
+            </div>
 
 
 
@@ -49,6 +57,7 @@
 	IMPORTS
 *\----------------------------------------------------------------------------------*/
 import axios from 'axios';
+import _ from 'lodash';
 
 
 /*----------------------------------------------------------------------------------*\
@@ -60,45 +69,64 @@ export default {
         return {
             title: '',
             description: '',
-            file: '',
+            files: [],
+            uploadFiles: [],
             message: '',
             error: false,
             uploading: false,
             uploadComplete: false,
-            progress: 0,    
+            progress: 0, 
+            uploadRdy: false   
         }
     },
     methods: {
         selectFile() {
-            const file = this.$refs.file.files[0];
-            console.log(file.type)
-            const allowedTypes = ["video/quicktime"]; // "image/jpeg", "image/png", "image/gif", 
-            const MAX_SIZE = 2000000000000;
-            const tooLarge = file.size > MAX_SIZE;
+            const files = this.$refs.files.files;
+            this.uploadFiles = [...this.uploadFiles, ...files];
 
-            if(allowedTypes.includes(file.type) && !tooLarge) {
-                this.file = file;
-                this.error = false;
-                this.message = "";
-            } else {
-                this.error = true;
-                this.message = tooLarge ? `The file is too large. Maz size is ${MAX_SIZE/1000}Kb` : "Only images are allowed"
+            this.files = [
+                ...this.files,
+                ..._.map(files, file => ({
+                    name: file.name,
+                    size: file.size,
+                    type: file.type,
+                    invalidMessage: this.validate(file)
+                }))
+            ]
+        },
+
+        validate(file) {
+            const MAX_SIZE = 2000000000000;
+            const allowedTypes = ["video/quicktime", "image/jpeg", "image/png"]; 
+
+            if(file.size > MAX_SIZE) {
+                return `Max size: ${MAX_SIZE/1024}Kb`;
             }
+
+            if(!allowedTypes.includes(file.type)) {
+                return "This format is not allowed";
+            }
+
+            return "";
         },
         async sendFile() {
             const formData = new FormData();
             formData.append('title', this.title);
             formData.append('description', this.description);
             formData.append('creator', this.$store.getters.user._id);
-            formData.append('file', this.file);
-
+            _.forEach(this.uploadFiles, file => {
+                if(this.validate(file) === "") {
+                    formData.append('files', file);
+                }
+            })
+           
             try {
                 this.uploading = true;
                 const res = await axios.post('/api/video/upload', formData, {
                     onUploadProgress: e => this.progress = Math.round(e.loaded * 100 / e.total)
                 })
-               console.log(res)
-                this.message = "File has been uploaded"
+                //console.log(res)
+                this.message = "Files has been uploaded"
                 this.file = "";
                 this.error = false;
                 this.uploading = false;
@@ -109,6 +137,35 @@ export default {
                 this.uploading = false;
                 this.uploadComplete = false;
             }
+        }
+    },
+    watch: {
+        files() {
+            const videos = ["video/quicktime"]; 
+            const images = ["image/jpeg", "image/png"];
+            let containMovie = false;
+            let containImg = false;
+            if(this.files.length > 2) {
+                this.message = "You need to only upload 1 video and 1 image"
+                return;
+            }
+
+            for(let i = 0; i < this.files.length; i++) {    
+                let testImg = images.includes(this.files[i].type);
+                let testVid = videos.includes(this.files[i].type);
+                if(testImg) {
+                    containImg = true;
+                }
+                if(testVid) {
+                    containMovie = true;
+                }
+            }
+
+            if(containMovie && containImg) {
+                
+                this.uploadRdy = true;
+            }
+            this.message = "";
         }
     }
 };
@@ -125,21 +182,46 @@ export default {
 .upload {
 
     &__form {
+        @include flexColumn(center, center);
         min-width: 500px;
 
+        .form__field {
+            width: 100%;
+            margin-bottom: 10px;
+
+            .field__input {
+                width: 100%;
+                padding: 10px 10px;
+            }
+
+            .field__textarea {
+                width: 100%;
+                resize: none;
+                height: 150px;
+                padding: 10px 10px;
+            }
+        }
+
+        .form__info {
+            color: getColor($darkTheme, fontColor);
+            font-size: 12px;
+            margin-bottom: 5px;
+        }
+
         .form__dropzone {
+            width: 100%;
             position: relative;
             @include flexRow(center, center);
             min-height: 200px;
             padding: 10px 10px;
             cursor: pointer;
-            outline: 2px dashed grey;
-            background-color: lightcyan;
-            color: dimgrey;
+            outline: 2px dashed getColor($darkTheme, fontColor);
+            background-color: getColor($accents, tertiary); 
+            color: getColor($darkTheme, fontColor);
             outline-offset: -10px;
 
             &:hover {
-                background-color: lightblue;
+                background-color: lighten(getColor($accents, tertiary), 5%);
             }
 
             .dropzone__input {
@@ -157,6 +239,38 @@ export default {
             .dropzone__progress {
                 
             }
+        }
+
+        .form__files {
+            @include flexRow(center, space-between);
+            padding: 5px;
+            margin-top: 5px;
+            width: 100%;
+            color: getColor($darkTheme, fontColor);
+
+            &--success {
+                color: getColor($accents, tertiary); 
+            }
+
+            &--error {
+                color: getColor($accents, primary);
+            }
+
+            .files__delete {
+                @extend %icon-btn;
+                color: getColor($darkTheme, fontColor);
+                font-size: 16px;
+
+                &:hover {
+                    color: getColor($accents, primary);
+                }
+            }
+        }
+
+        .form__submit {
+            @extend %secondary-btn;
+            margin-top: 20px;
+
         }
     }
 }
